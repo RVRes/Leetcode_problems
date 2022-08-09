85e72d616df608c9bad80b85a90b93e05668e10f
scorpion <rv.reshetnikov@gmail.com>
2022-08-08 23:43:32 -0500

    Added max_profit.py implementation using operations.itemgetter

--- max_profit.py
+++ max_profit.py
@@ -1,9 +1,6 @@
 # в списке изменение цены во времени, найти лучшую сделку (минимальная цена покупки, максимальная цена продажи)
 # обратить внимание, что мы не можем продать раньше, чем купить
-
-
-
-stocks = [1, 10, 5, 6]
+from operator import itemgetter
 
 
 def max_profit2(stocks):
@@ -19,5 +16,20 @@ def max_profit3(stocks):
     return max([max(list(stocks[ind::])) - el for ind, el in enumerate(stocks)])
 
 
-print(max_profit2(stocks))
-print(max_profit3(stocks))
+def max_profit_4(stocks):
+    stocks_range = [(stocks[i], stocks[j], stocks[i]-stocks[j]) for i in range(1, len(stocks)) for j in range(i)]
+    return max(stocks_range, key=itemgetter(2))
+
+
+stocks_ = [
+    [1, 10, 5, 6],
+    [6, 5, 10, 11, 3, 8, 7, 9, 8, 2, 7, 8]
+]
+
+
+for stock in stocks_:
+    print(max_profit2(stock))
+    print(max_profit3(stock))
+    best_deal = max_profit_4(stock)
+    print(f'highest difference: {best_deal[2]}, buy price: {best_deal[0]}, sell price: {best_deal[1]} ')
+
